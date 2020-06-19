2a72cb810a71a183b6f6f6976baf6b7083f33371
ngiannantonio <45762535+ngiannantonio@users.noreply.github.com>
2020-06-19 00:27:24 -0300

    Entrega Clase 08 y Clase 09

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,39 @@
+// [] Array
+// {} Objeto
+
+// Precios Provincias
+var amountProvinceCABA = 500;
+var amountProvinceBS = 500;
+var amountProvinceSF = 2500;
+
+// Precios Tipo de Camión
+var amountTruckSM = 100;
+var amountTruckMD = 200;
+var amountTruckXL = 300;
+
+// Precios Tipo de Servicio
+var amountServiceEmpaque = 50;
+var amountServiceDesempaque = 50;
+var amountServiceAmbos = 100;
+
+// Precio Total
+var totalPrice;
+
+// Objeto Completo
+var totalData = {};
+
+// Steps
 var showing = [1, 0, 0, 0, 0, 0, 0];
 var steps = ['step0', 'step1', 'step2', 'step3', 'step4', 'step5', 'step6'];
+
 function next() {
+
     var qElems = [];
+
     for (var i = 0; i < steps.length; i++) {
         qElems.push(document.getElementById(steps[i]));
     }
+
     for (var i = 0; i < showing.length; i++) {
         if (showing[i] == 1) {
             qElems[i].style.display = 'none';
@@ -19,93 +48,144 @@ function next() {
             break;
         }
     }
+
+    // Step0
+    totalData.FirstName = document.getElementById("inputFirstName").value;
+    totalData.LastName = document.getElementById("inputLastName").value;
+    totalData.Mail = document.getElementById("inputMail").value;
+
+    // console.log(totalData);
+
+    let elementsUserName = document.querySelectorAll(".userName");
+    console.log(elementsUserName);
+    elementsUserName.forEach(x => {
+        x.innerHTML = totalData.FirstName;
+    });
+
+    // var list = document.querySelectorAll('.userName');
+    // for (var n = 0; n < list.length; ++n) {
+    //     list[n].innerHTML = totalData.FirstName;
+    //     console.log(list[n]);
+    // }
+
+    // Step1
+    totalData.CheckInStreet = document.getElementById("inputCheckInStreet").value;
+    totalData.CheckInNumber = document.getElementById("inputCheckInNumber").value;
+    totalData.CheckInFloor = document.getElementById("inputCheckInFloor").value;
+    totalData.CheckInPostalCode = document.getElementById("inputCheckInPostalCode").value;
+    totalData.CheckInLocation = document.getElementById("selectCheckInLocation").value;
+    totalData.CheckInProvince = document.getElementById("selectCheckInProvince").value;
+
+    // console.log(totalData.CheckInProvince);
+
+    let elementsCheckInAddress = document.querySelectorAll(".checkInAddress");
+    // console.log(elementsCheckInAddress);
+    elementsCheckInAddress.forEach(x => {
+        x.innerHTML = totalData.CheckInStreet.concat(totalData.CheckInNumber,totalData.CheckInFloor);
+    });
+
+    if(totalData.CheckInProvince == 1) {
+        totalPrice = amountProvinceCABA;
+    } else if(totalData.CheckInProvince == 2) {
+        totalPrice = amountProvinceCABA;
+    } else if(totalData.CheckInProvince == 3) {
+        totalPrice = amountProvinceSF;
+    }
+
+    // console.log(totalPrice);
+
+    // Step2
+    totalData.CheckInDate = document.getElementById("inputCheckInDate").value;
+    totalData.CheckInTime = document.getElementById("inputCheckInTime").value;
+
+    let elementsCheckInDate = document.querySelectorAll(".checkInDate");
+    // console.log(elementsCheckInDate);
+    elementsCheckInDate.forEach(x => {
+        x.innerHTML = totalData.CheckInDate.concat(totalData.CheckInTime);
+    });
+
+    // console.log(totalData);
+
+    // Step3
+    totalData.CheckOutStreet = document.getElementById("inputCheckOutStreet").value;
+    totalData.CheckOutNumber = document.getElementById("inputCheckOutNumber").value;
+    totalData.CheckOutFloor = document.getElementById("inputCheckOutFloor").value;
+    totalData.CheckOutPostalCode = document.getElementById("inputCheckOutPostalCode").value;
+    totalData.CheckOutLocation = document.getElementById("selectCheckOutLocation").value;
+    totalData.CheckOutProvince = document.getElementById("selectCheckOutProvince").value;
+
+    // console.log(totalData.CheckOutProvince);
+
+    let elementsCheckOutAddress = document.querySelectorAll(".checkOutAddress");
+    // console.log(elementsCheckOutAddress);
+    elementsCheckOutAddress.forEach(x => {
+        x.innerHTML = totalData.CheckOutStreet.concat(totalData.CheckOutNumber,totalData.CheckOutFloor);
+    });
+
+    if(totalData.CheckOutProvince == 1) {
+        totalPrice = totalPrice + amountProvinceCABA;
+    } else if(totalData.CheckOutProvince == 2) {
+        totalPrice = totalPrice + amountProvinceCABA;
+    } else if(totalData.CheckOutProvince == 3) {
+        totalPrice = totalPrice + amountProvinceSF;
+    }
+
+    // console.log(totalPrice);
+
+    // Step4
+    totalData.CheckOutDate = document.getElementById("inputCheckOutDate").value;
+    totalData.CheckOutTime = document.getElementById("inputCheckOutTime").value;
+
+    let elementsCheckOutDate = document.querySelectorAll(".checkOutDate");
+    // console.log(elementsCheckOutDate);
+    elementsCheckOutDate.forEach(x => {
+        x.innerHTML = totalData.CheckOutDate.concat(totalData.CheckOutTime);
+    });
+
+    // console.log(totalData);
+
+    // Step5
+    totalData.TruckType = 1;
+
+    let elementsTruckType = document.querySelectorAll(".truckType");
+    // console.log(elementsTruckType);
+    elementsTruckType.forEach(x => {
+        x.innerHTML = totalData.TruckType;
+    });
+
+    if(totalData.TruckType == 1) {
+        totalPrice = totalPrice + amountTruckSM;
+    } else if(totalData.TruckType == 2) {
+        totalPrice = totalPrice + amountTruckMD;
+    } else if(totalData.TruckType == 3) {
+        totalPrice = totalPrice + amountTruckXL;
+    }
+
+    // console.log(totalPrice);
+
+    // Step6
+    totalData.ServiceType = 1;
+
+    let elementsServiceType = document.querySelectorAll(".serviceType");
+    // console.log(elementsServiceType);
+    elementsServiceType.forEach(x => {
+        x.innerHTML = totalData.ServiceType;
+    });
+
+    if(totalData.ServiceType == 1) {
+        totalPrice = totalPrice + amountServiceEmpaque;
+    } else if(totalData.ServiceType == 2) {
+        totalPrice = totalPrice + amountServiceDesempaque;
+    } else if(totalData.ServiceType == 3) {
+        totalPrice = totalPrice + amountServiceAmbos;
+    } else if(totalData.ServiceType == 4) {
+        totalPrice;
+    } 
+
+    // console.log(totalPrice);
+
+    // Monto Total
+    let elementsPrice = document.getElementById("totalPrice");
+    elementsPrice.innerHTML = totalPrice;
+
 }
-
-alert("¡Bienvenido! Primero queremos conocerte...");
-
-var inputFirstName = prompt("¿Cuál es tu nombre?");
-var inputLastName = prompt("¿Cuál es tu apellido?");
-var inputMail = prompt("¿Cuál es tu correo electrónico?");
-
-alert("Hola " + inputFirstName + "! ¿A dónde te vas a mudar hoy? Primero ingresa los datos de origen de la mudanza. ¿Cuál es la dirección de origen?");
-
-var checkInData = [];
-var checkOutData = [];
-
-var inputCheckInStreet = prompt("¿Cuál es la calle o avenida de origen?");
-var inputCheckInNumber = prompt("¿Cuál es el número de origen?");
-var inputCheckInFloor = prompt("¿Cuál es el piso y/o departamento de origen?");
-var inputCheckInPostalCode = prompt("¿Cuál es el código postal de origen?");
-var selectCheckInLocation = prompt("¿Cuál es la localidad de origen?");
-var selectCheckInProvince = prompt("¿Cuál es la provincia de origen?");
-var inputCheckInDate = prompt("¿Cuál es la fecha de origen?");
-var inputCheckInTime = prompt("¿Cuál es la hora de origen?");
-
-alert("¡Muy bien " + inputFirstName + "! Ahora debes ingresar los datos de destino de la mudanza. ¿Cuál es la dirección de destino?");
-
-var inputCheckOutStreet = prompt("¿Cuál es la calle o avenida de destino?");
-var inputCheckOutNumber = prompt("¿Cuál es el número de destino?");
-var inputCheckOutFloor = prompt("¿Cuál es el piso y/o departamento de destino?");
-var inputCheckOutPostalCode = prompt("¿Cuál es el código postal de destino?");
-var selectCheckOutLocation = prompt("¿Cuál es la localidad de destino?");
-var selectCheckOutProvince = prompt("¿Cuál es la provincia de destino?");
-var inputCheckOutDate = prompt("¿Cuál es la fecha de destino?");
-var inputCheckOutTime = prompt("¿Cuál es la hora de destino?");
-
-alert("¡Excelente! ya falta poco...");
-
-var selectTruckType = prompt("¿Qué tipo de camión necesitas? (Chico o Grande o Extra Grande)");
-
-alert("¡Último paso " + inputFirstName + "!");
-
-var selectServiceType = prompt("¿Qué servicio adicional necesitas? (Empaque o Desempaque o Ambos o Ninguno)");
-
-checkInData.push(inputCheckInStreet, inputCheckInNumber, inputCheckInFloor, inputCheckInPostalCode, selectCheckInLocation, inputCheckInDate, inputCheckInTime);
-checkOutData.push(inputCheckOutStreet, inputCheckOutNumber, inputCheckOutFloor, inputCheckOutPostalCode, selectCheckOutLocation, inputCheckOutDate, inputCheckOutTime);
-
-console.log("Datos del Origen: " + checkInData + "; Datos del Destino: " + checkOutData + "; Tipo de Camión: " + selectTruckType + "; Servicios Adicionales: " + selectServiceType);
-
-var concatArray = (checkInData,checkOutData) => {
-   let order = checkInData.concat(checkOutData, selectTruckType, selectServiceType, inputFirstName);
-   return order;
-}
-
-var order = concatArray(checkInData,checkOutData);
-
-console.log(order);
-
-alert(`¡${order[16]} confirmá que los datos esten bien! ORIGEN == Dirección: ${order[0]} ${order[1]} ${order[2]} ${order[3]} ${order[4]}, Fecha: ${order[5]} ${order[6]}, DESTINO == Dirección: ${order[7]} ${order[8]} ${order[9]} ${order[10]} ${order[11]}, Fecha: ${order[12]} ${order[13]}, Camión: ${order[14]}, Servicio Adicional: ${order[15]}`);
-
-var successfullyCompleted = prompt("¿Aceptas el presupuesto? (1:Sí o 2:No)");
-
-var yes = 1;
-var no = 2;
-
-var convertNumber = Number(successfullyCompleted);
-
-if(successfullyCompleted == 1) {
-   alert("¡Felicitaciones " + inputFirstName + "! Tu mudanza ha sido confirmada.");
-} else if(successfullyCompleted == 2) {
-   alert("¡No hay problema " + inputFirstName + "! Volve a intentar más tarde.");
-} else {
-   prompt(inputFirstName + " intentá ingresando 1 para Sí o 2 para No.");
-}
-
-console.log(successfullyCompleted);
-
-// No se como hacer para presupuestar.
-// // precio total de los productos de la mudanza
-// function ShoppingCart() {
-//     var listShoppingCart = [];
-//     this.total = function () {
-//         var total = 0;
-//         listShoppingCart.forEach(function (currentProduct) {
-//             total = currentProduct.price * currentProduct.quantity + total;
-//         })
-//         return total;
-//     }
-// }
-// var myShoppingCart = new ShoppingCart();
-// // obtengo la suma de los precios de los elementos de la mudanza y la muestro por consola
-// var priceBudget = myShoppingCart.total();
-// console.log(`Total: ${priceBudget}`);
